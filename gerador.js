0f841efacac8f6a6ca212f46d5de58ffd5e0c9b0
Matheus Felipe <matheus_felipe1207@hotmail.com>
2025-05-17 18:33:03 -0300

    Update gerador.js

--- gerador.js
+++ gerador.js
@@ -6,5 +6,30 @@ const rl = readline.createInterface({
 });
 
 function gerarsenha(tamanho, opcoes) {
-    
+    const letrasmaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const letrasminusculas = 'abcdefghijklmnopqrstuvwxyz';  
+    const numeros = '0123456789';
+    const simbolos = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+    let caracteres = '';
+    if (opcoes.maiusculas) caracteres += letrasmaiusculas;
+    if (opcoes.letrasminusculas) caracteres += letrasminusculas;
+    if (opcoes.numeros) caracteres += numeros;
+    if (opcoes.simbolos) caracteres += simbolos;
+
+    if (!caracteres) {
+        return 'Erro: Nenhuma opção de caractere selecionada.';
+    }
+
+    let senha = '';
+    for (let i = 0; i < tamanho; i++) {
+        const index = Math.floor(Math.random() * caracteres.length);
+        senha += caracteres.charAt(index);
+    }
+
+    return senha;
+}
+
+function perguntar() {
+    rl.question('Qual é o tamanho da senha? ', (tamanhoStr) => {const}
 }
